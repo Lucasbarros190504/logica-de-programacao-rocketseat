a6f4290de766f63252b91b8595d426d4ad765e9f
lucas barros <lucasbarrosjl04@gmail.com>
2026-01-08 20:41:43 -0300

    feat: cria quiz interativo em Python

--- atividades-python/atividade_quiz.py
+++ atividades-python/atividade_quiz.py
@@ -1,20 +1,35 @@
+# Lista de perguntas e respostas do quiz
 perguntas = [
-  ['Qual o nome do atual presidente do Brasil?,' 'Lula'] ,
-  ['Qual o ano da proxima copa do mundo?,' '2026'] ,
-  ['Qual a montanha mais alta do mundo?,' 'monte everest'] , 
-  ['Qual país tem o formato de uma bota?,' 'Itália'] ,
-
+  ['Qual o nome do atual presidente do Brasil?' , 'Lula'] ,
+  ['Qual o ano da proxima copa do mundo?' , '2026'] ,
+  ['Qual a montanha mais alta do mundo?' , 'monte everest'] , 
+  ['Qual país tem o formato de uma bota?' , 'Italia'] ,
 ]
+# Contador de respostas corretas
+acertos = 0
 
-print('Olá! Seja bem vindo ao quiz LB')
-print('O quiz terá o total de 5 perguntas e cada pergunta irá valer 2 pontos. Vamos ver quantos pontos você vai conseguir fazer. \nVamos nessa!')
-input('digite ENTER para começar o quis')
+# Início do quiz
+print('Olá! Seja bem-vindo ao Quiz LB')
+print('O quiz terá 4 perguntas. Vamos ver quantas você consegue acertar!. \nVamos nessa!')
+input('Digite ENTER para começar o quiz')
 
+# Percorre cada pergunta da lista
 for pergunta_resposta in perguntas:
+
+  # Separa a pergunta e a resposta correta
   pergunta = pergunta_resposta[0]
-  #resposta_correta  = pergunta_resposta[1] # Você usará isso para verificar a resposta depois
-
+  resposta_correta  = pergunta_resposta[1].lower()
+  
+  # Mostra a pergunta e lê a resposta do usúario
   print(f"\nPergunta: {pergunta}")
-  resposta_usuario = input("Sua resposta: ")
+  resposta_usuario = input("Sua resposta: ").lower()
 
+  # Verifica se a resposta está correta
+  if resposta_usuario == resposta_correta:
+    print('Resposta correta! ✅')
+    acertos += 1
+  else:
+    print(f'Resposta errada!❌ A resposta correta era: {resposta_correta}')
 
+print('\nQuiz finalizado!')
+print(f'Você acertou {acertos} de {len(perguntas)} perguntas.')
